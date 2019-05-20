08519febf9488c5fc26d44434bbe1d09f8db0a16
Phoenicis <alliefirebaugh@gmail.com>
2019-05-20 04:57:17 -0400

    Updates

---
+++ precache-manifest.41c77f78cb90caf45665baea3792a45c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14364a9706700280f723b7579a787612",
+    "url": "/Endless-SS-Exercise/index.html"
+  },
+  {
+    "revision": "daac3b2f4f87eb27233f",
+    "url": "/Endless-SS-Exercise/static/css/main.d3efa2fc.chunk.css"
+  },
+  {
+    "revision": "362958da272f928d2e12",
+    "url": "/Endless-SS-Exercise/static/js/2.09b6d750.chunk.js"
+  },
+  {
+    "revision": "daac3b2f4f87eb27233f",
+    "url": "/Endless-SS-Exercise/static/js/main.8fe84f0c.chunk.js"
+  },
+  {
+    "revision": "de103f2c5059626b40ce",
+    "url": "/Endless-SS-Exercise/static/js/runtime~main.e4aa4aec.js"
+  },
+  {
+    "revision": "6e24790bf1a20dfee3fd5584801d3204",
+    "url": "/Endless-SS-Exercise/static/media/logoEndless.6e24790b.svg"
+  },
+  {
+    "revision": "ccd3c74c83d1df9cb84da4d9ea237c60",
+    "url": "/Endless-SS-Exercise/static/media/photo-couch.ccd3c74c.jpg"
+  }
+]);
